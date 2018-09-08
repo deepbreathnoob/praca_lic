f9ace9130a07e2902531b818cedd284fc7839e62
deepbreathnoob <kuciel.pawel@gmail.com>
2018-09-09 00:58:32 +0200

    zmiana parametrów zatrzymania

--- player.gd
+++ player.gd
@@ -39,7 +39,7 @@ func _physics_process(delta):
 		else:
 			$Sprite.play("Fall")
 		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.09)
+			motion.x = lerp(motion.x, 0, 0.05)
 	
 	motion = move_and_slide(motion, UP)
 	
