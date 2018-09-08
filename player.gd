44ba13ea784c53fe7a25f70355d8350b3da5032e
deepbreathnoob <kuciel.pawel@gmail.com>
2018-09-09 00:57:49 +0200

    zmiana parametrów stop

--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 const UP = Vector2(0,-1);
-const GRAVITY = 21
+const GRAVITY = 20
+
 const MAX_SPEED = 205
 const ACCELERATION = 50
 const JUMP_HEIGHT = -555
@@ -25,20 +26,20 @@ func _physics_process(delta):
 	else:
 		$Sprite.play("Idle")
 		friction = true
-		motion.x = lerp(motion.x, 0, 0.3)
+		motion.x = lerp(motion.x, 0, 0.4)
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
 		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.3)
+			motion.x = lerp(motion.x, 0, 0.4)
 	else:
 		if motion.y < 0:
 			$Sprite.play("Jump")
 		else:
 			$Sprite.play("Fall")
 		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.05)
+			motion.x = lerp(motion.x, 0, 0.09)
 	
 	motion = move_and_slide(motion, UP)
 	
