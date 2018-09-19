a4e3deaa5ebfa0446ce82be49866c03b7b24a97f
deepbreathnoob <kuciel.pawel@gmail.com>
2018-09-19 02:49:25 +0200

    modyfikacja kodu, zmiennych

--- player.gd
+++ player.gd
@@ -1,26 +1,26 @@
 extends KinematicBody2D
 
 const UP = Vector2(0,-1);
-const GRAVITY = 20
+const GRVTY = 20
 
-const MAX_SPEED = 205
-const ACCELERATION = 50
-const JUMP_HEIGHT = -555
+const MAX_SPD = 205
+const ACC = 50
+const JUMP_H = -555
 var motion = Vector2()
 
 
 
 func _physics_process(delta):
-	motion.y += GRAVITY
+	motion.y += GRVTY
 	
 	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = min(motion.x + ACCELERATION,MAX_SPEED)
+		motion.x = min(motion.x + ACC,MAX_SPD)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		motion.x = max(motion.x - ACC, -MAX_SPD)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
@@ -30,7 +30,7 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
-			motion.y = JUMP_HEIGHT
+			motion.y = JUMP_H
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.3)
 	else:
